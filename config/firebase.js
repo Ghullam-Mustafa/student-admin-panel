e48f541bd97a709aa43721caae80bc4e8f662e06
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-11-09 22:33:21 +0500

    change project

--- config/firebase.js
+++ config/firebase.js
@@ -7,14 +7,27 @@ import { getFirestore } from "firebase/firestore";3
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBeNFg_LspMqTojb0sDcUec8cohVogQvCM",
-  authDomain: "students-admin-panel.firebaseapp.com",
-  projectId: "students-admin-panel",
-  storageBucket: "students-admin-panel.appspot.com",
-  messagingSenderId: "1079013840194",
-  appId: "1:1079013840194:web:eed99584762fcf381d24db"
+  apiKey: "AIzaSyAILBxpPDYAU3rvuBgejzbVLYqXmLfeFmw",
+  authDomain: "lms-crud-812ea.firebaseapp.com",
+  projectId: "lms-crud-812ea",
+  storageBucket: "lms-crud-812ea.appspot.com",
+  messagingSenderId: "298884123828",
+  appId: "1:298884123828:web:e235454d3e26d7062d5be1"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAILBxpPDYAU3rvuBgejzbVLYqXmLfeFmw",
+//   authDomain: "lms-crud-812ea.firebaseapp.com",
+//   projectId: "lms-crud-812ea",
+//   storageBucket: "lms-crud-812ea.appspot.com",
+//   messagingSenderId: "298884123828",
+//   appId: "1:298884123828:web:e235454d3e26d7062d5be1"
+// };
+
